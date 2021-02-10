1b4cd99b39c65fb271ad936eb7d944634d0bf9b1
TejasSC <tsctejas@gmail.com>
2021-02-10 19:15:15 +0000

    remove unnecessary button and complete first navbar

--- the-website/src/components/Navbar.js
+++ the-website/src/components/Navbar.js
@@ -1,31 +1,19 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TRVL <i className='fab fa-typo3' />
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Tejas Chandrasekar <i className='fab fa-typo3' />
+
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -46,17 +34,11 @@ function Navbar() {
                 Music
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links-mobile' onClick={closeMobileMenu}>
-                Sign up
-              </Link>
-            </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>
   );
 }
 
-export default Navbar
+export default Navbar;
