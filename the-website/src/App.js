6d09b38e7d256d05ab56f41dc16530164eb37b0e
TejasSC <tsctejas@gmail.com>
2021-02-17 23:11:49 +0000

    Add placeholder pages for website

--- the-website/src/App.js
+++ the-website/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Home from './components/pages/Home';
 import Music from './components/pages/Music';
 import Projects from './components/pages/Projects';
+import About from './components/pages/About';
 import SignUp from './components/pages/SignUp';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/projects' exact component={Projects} />
           <Route path='/music' exact component={Music} />
+          <Route path='/about' exact component={About} />
           <Route path='/sign-up' exact component={SignUp} />
         </Switch>
       </Router>
