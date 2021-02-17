2b8cb1446de287facbfa72f9b06efa60377edf83
TejasSC <tsctejas@gmail.com>
2021-02-17 10:51:58 +0000

    fix mobile menu and footer styling issues, but need to fix cards alignment problems

--- the-website/src/App.js
+++ the-website/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Music from './components/pages/Music';
+import Projects from './components/pages/Projects';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,11 +13,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component=
-          {Home} />
+          <Route path='/' exact component={Home} />
+          <Route path='/projects' exact component={Projects} />
+          <Route path='/music' exact component={Music} />
+          <Route path='/sign-up' exact component={SignUp} />
         </Switch>
       </Router>
-
     </>
   );
 }
